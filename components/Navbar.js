0b14c78bc95386e3c76ec909e3b754c0dcd8b306
VISHAL MISHRA <beyondinfinity9988@gmail.com>
2023-04-15 07:49:18 -0700

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Navbar() {
+ return (
+
+    <>
+
+   <div className=''>
+
+    <div>
+        <h1 className='font-abc text-3xl   pl-5 bg-black'> $7,500 tax credit for Model Y and Model 3. Model 3 RWD credit reducing to $3,750 on April 18.
+        <a href="#" className='inline underline cursor-pointer '> Learn more  </a>
+ </h1>
+ <div className="">
+ <a href="#"> TESLA  </a>
+
+ <a href="#">Model S  </a>
+ <a href="#"> Model S  </a>
+ <a href="#" >Model S </a>
+ <a href="#">Model S </a>
+ <a href="#"> Solar panels </a>
+ <a href="#"> Solar Roofs </a>
+ <a href="#"> Powerwall</a>
+ <a href="#"> Shop </a>
+ <a href="#"> Account </a>
+ <a href="#"> Menu</a>
+
+ </div>
+
+
+    </div>
+   </div>
+   </>
+ )
+}
